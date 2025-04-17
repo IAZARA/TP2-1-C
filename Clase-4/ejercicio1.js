3614e2a5a4db90a06b69a6030e26fe962b5b6d5c
Matias <matias.duro@ina.edu.ar>
2025-04-16 21:42:27 -0300

    clase 4

--- Clase-4/ejercicio1.js
+++ Clase-4/ejercicio1.js
@@ -1,4 +1,4 @@
-// Crear funcion asincronica, que lea user.json, 
+// lean user.json, 
 // y les agregue al mismo, los nuevos usuarios, tambien actualizando el total de usuarios
 // y agregarle a cada objeto del usuario, la key aprobado con valor en true
 
