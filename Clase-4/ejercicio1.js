fbcbf61879a4f04440523df02b8e275787b81179
Matias <matias.duro@ina.edu.ar>
2025-04-16 21:54:33 -0300

    clase 4

--- Clase-4/ejercicio1.js
+++ Clase-4/ejercicio1.js
@@ -2,4 +2,20 @@
 // y les agregue al mismo, los nuevos usuarios, tambien actualizando el total de usuarios
 // y agregarle a cada objeto del usuario, la key aprobado con valor en true
 
-
+let nuevosUsers = {
+    respuesta: true,
+    usuarios: [
+        {
+            "name": "Lionel",
+            "email": "lionel@example.com"
+          },
+          {
+            "name": "Martin",
+            "email": "martin@example.com"
+          },
+          {
+            "name": "Ana Laura",
+            "email": "ana.laura@example.com"
+          }
+    ]
+}
